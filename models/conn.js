4542a2b3448a391b75f2adb2e9e3dd973c1d3d26
Gokul Modi <40318556+Goku56@users.noreply.github.com>
2024-08-15 18:19:29 +0530

    Update conn.js

--- models/conn.js
+++ models/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB)
+mongoose.connect('mongodb://localhost:27017/node_git_jenkins')
     .then(()=>{
         console.log('mongodb connected')
     }).catch((err)=>{
